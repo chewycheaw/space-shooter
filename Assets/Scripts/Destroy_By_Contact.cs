6d916f90c538fce5978bf4b032a515a88128c784
Jamie <jamiecheaw@gmail,com>
2018-10-30 23:47:53 -0400

    initial commit

---
+++ Assets/Scripts/Destroy_By_Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_By_Contact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+       Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
